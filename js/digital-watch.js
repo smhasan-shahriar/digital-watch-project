ebd4eea8621f74b76e9e9b27cecdbf1f8c180fa1
Ador Shahriar <ador.shahriar@gmail.com>
2023-08-31 12:12:08 +0600

    stopwatch with minute and hours

--- js/digital-watch.js
+++ js/digital-watch.js
@@ -1,25 +1,74 @@
-const countNowField = document.getElementById('countdown');
-let intervalId; 
+const countNowField = document.getElementById('seconds');
+const countMinuteField = document.getElementById('minutes');
+const countHourField = document.getElementById('hours');
+let intervalIdSecond; 
+let intervalIdMinute; 
+let intervalIdHour; 
 
 const countStart = (stopButton) => {
     let countNow = parseInt(countNowField.innerText);
-    intervalId = setInterval(() => {
-        countNowField.innerText = countNow++;
-    }, 1000)
+    let countMinute = parseInt(countMinuteField.innerText);
+    let countHour= parseInt(countHourField.innerText);
+    intervalIdSecond = setInterval(() => {
+        countNowField.innerText = ++countNow;
+        if(countNow > 9){
+            document.getElementById('prefix-sec').classList.add('hidden');
+        }
+        else{
+            document.getElementById('prefix-sec').classList.remove('hidden');
+        }
+
+        if(countNow === 59){
+            countNow = 0;
+        }
+    }, 10)
+    intervalIdMinute = setInterval(()=>{
+        countMinuteField.innerText = ++countMinute;
+        if(countMinute > 9){
+            document.getElementById('prefix-min').classList.add('hidden');
+        }
+        else{
+            document.getElementById('prefix-min').classList.remove('hidden');
+        }
+
+        if(countMinute === 59){
+            countMinute = 0;
+        }
+    }, 600)
+    intervalIdHour = setInterval(()=>{
+        countHourField.innerText = ++countHour;
+        if(countHour > 9){
+            document.getElementById('prefix-hour').classList.add('hidden');
+        }
+        else{
+            document.getElementById('prefix-hour').classList.remove('hidden');
+        }
+
+        if(countHour === 23){
+            countHour = 0;
+        }
+    }, 36000)
+    
 
 
 
 }
 
 const countStop = () => {
-    console.log(intervalId);
-    clearInterval(intervalId);
+    clearInterval(intervalIdSecond);
+    clearInterval(intervalIdMinute);
+    clearInterval(intervalIdHour);
 
 }
 
 const countReset = () => {
     countStop();
     countNowField.innerText = 0; 
+    countMinuteField.innerText = 0; 
+    countHourField.innerText = 0; 
+    document.getElementById('prefix-sec').classList.remove('hidden');
+    document.getElementById('prefix-min').classList.remove('hidden');
+    document.getElementById('prefix-hour').classList.remove('hidden');
 }
 
 
